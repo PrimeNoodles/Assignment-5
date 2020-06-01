bfe189b1d86dda692d8a55839616fb9d6837be2a
Manahil Sher <manahil.sher@gmail.com>
2020-06-01 18:00:06 -0400

    created a table and add-rows button

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let numColumns = 3;
+let numRows = 1;
 
+function addRow() {
+    let table = document.getElementsByTagName("tbody")[0];
+    let row = document.createElement("tr");
+    for (let i = 0; i < numColumns; i++) {
+        let column = document.createElement("td");
+        row.appendChild(column);
+    }
+    table.appendChild(row);
+}
