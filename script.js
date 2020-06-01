dbc2eff97933556dbd49b3c05486bc98aff41acc
Manahil Sher <manahil.sher@gmail.com>
2020-06-01 18:14:51 -0400

    add remove-rows feature

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let numColumns = 3;
+let numRows = 1;
+let table = document.getElementsByTagName("tbody")[0];
 
+function addRow() {
+    let row = document.createElement("tr");
+    for (let i = 0; i < numColumns; i++) {
+        let column = document.createElement("td");
+        row.appendChild(column);
+    }
+    table.appendChild(row);
+}
+
+function removeRow() {
+    let row = table.lastChild;
+    table.removeChild(row);
+}
